45a183dec24e62d500e309fe5f6b2fb6a7d07080
Dennis Schad <dennis.schad1991@gmail.com>
2024-03-12 16:30:24 +0100

    Implementation Domain, Web, Mock Persistence. Add Several Test cases

---
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -0,0 +1,34 @@
+package com.polarbookshop.catalogservice.web;
+
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService service;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception {
+        String isbn = "1234567891";
+        given(service.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+
+}
