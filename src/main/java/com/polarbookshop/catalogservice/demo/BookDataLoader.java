291a7588aecf0acde2f550256f1a8502b3c2b670
Dennis Schad <dennis.schad1991@gmail.com>
2024-03-22 12:08:10 +0100

    Add data persistence

---
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,27 @@
+package com.polarbookshop.catalogservice.demo;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Profile("testdata")
+@RequiredArgsConstructor
+public class BookDataLoader {
+
+    private final BookRepository bookRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadTestData() {
+        bookRepository.deleteAll();
+        var book1 = Book.of("1234567890", "Northern Lights", "Lyra Silverstar", 9.90);
+        var book2 = Book.of("1234567891", "Polar Journey", "Iorek Polarson", 12.90);
+        bookRepository.saveAll(List.of(book1, book2));
+    }
+}
